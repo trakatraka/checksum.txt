90de156cc0a749a0d8dd2c7be556c82be48f7511
user <user@localhost>
2024-10-22 04:50:38 -0300

    minor refacgor

--- src/main.py
+++ src/main.py
@@ -21,10 +21,10 @@ if args.CMD[0] == "sync":
     if args.target == None or args.source == None:
         error(f"--source and --target must be specified")
         quit(1)
+    elif args.PATHS != None:
+        error(f"PATHS cannot be specified with sync command")
+        quit(1)
     else:
-        if args.PATHS != None:
-            error(f"PATHS cannot be specified with sync command")
-            quit(1)
         sync(args)
 else:
     if args.target != None or args.source != None:
