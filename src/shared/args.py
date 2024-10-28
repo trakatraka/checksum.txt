9dc3bcbfaa774185e2482beba6ef384d5e9f6946
user <user@localhost>
2024-10-28 19:32:10 -0300

    minor refactor

--- src/shared/args.py
+++ src/shared/args.py
@@ -12,14 +12,18 @@ parser = ArgumentParser(
     prog='checksum.txt',
     formatter_class=ArgumentDefaultsHelpFormatter)
 
-parser.add_argument('--pre-sync', nargs='?', choices=["all", "source", "target", "none"], default="all", help='the pre sync setting for %(prog)s (default: %(default)s)')
-parser.add_argument('--sync-mode', nargs='?', choices=["all", "delete", "replace", "add"], default="all", help='the sync setting for %(prog)s (default: %(default)s)')
-parser.add_argument('--verbose', nargs='?', type=int, default=0, help='the verbose setting for %(prog)s (default: %(default)s)')
-parser.add_argument('--print-every', nargs='?', type=int, default=1000, help='the print every setting for %(prog)s (default: %(default)s)')
-parser.add_argument('--source', nargs='?', type=str, default=None, help='the source setting for sync %(prog)s (default: %(default)s)')
-parser.add_argument('--target', nargs='?', type=str, default=None, help='the target setting for sync %(prog)s (default: %(default)s)')
-parser.add_argument('--dry-run', nargs='?', type=int, default=0, help='the dry-run setting for %(prog)s (default: %(default)s)')
-parser.add_argument('--checksum', nargs='?', type=str, default="./checksum.txt", help='the checksum.txt file path (default: %(default)s)')
-parser.add_argument('--do-not-scan', nargs='?', type=int, default=0, help='disable scanning for new files (default: %(default)s)')
 parser.add_argument('CMD', nargs=1, choices=['sync', 'validate', 'append', 'changes', 'override'], help='the command to run')
 parser.add_argument('PATHS', nargs='?', type=list_of_paths, default=None, help='a comma separated list of paths %(prog)s (default: %(default)s)')
+
+parser.add_argument('--checksum', nargs='?', type=str, default="./checksum.txt", help='the checksum.txt file path (default: %(default)s)')
+
+parser.add_argument('--pre-sync', nargs='?', choices=["all", "source", "target", "none"], default="all", help='the pre sync setting for %(prog)s (default: %(default)s)')
+parser.add_argument('--sync-mode', nargs='?', choices=["all", "delete", "replace", "add"], default="all", help='the sync setting for %(prog)s (default: %(default)s)')
+parser.add_argument('--source', nargs='?', type=str, default=None, help='the source setting for sync %(prog)s (default: %(default)s)')
+parser.add_argument('--target', nargs='?', type=str, default=None, help='the target setting for sync %(prog)s (default: %(default)s)')
+
+parser.add_argument('--verbose', nargs='?', type=int, default=0, help='the verbose setting for %(prog)s (default: %(default)s)')
+parser.add_argument('--print-every', nargs='?', type=int, default=1000, help='the print every setting for %(prog)s (default: %(default)s)')
+
+parser.add_argument('--dry-run', nargs='?', type=int, default=0, help='the dry-run setting for %(prog)s (default: %(default)s)')
+parser.add_argument('--do-not-scan', nargs='?', type=int, default=0, help='disable scanning for new files (default: %(default)s)')
